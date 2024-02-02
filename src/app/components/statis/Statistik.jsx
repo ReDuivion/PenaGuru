0bb704213418a64d68f7a516a6a9992ac0d33da0
Luiforr <rizkyrofiul@gmail.com>
2024-02-03 00:30:58 +0700

    Luifor

---
+++ src/app/components/statis/Statistik.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import {Textarea} from "@nextui-org/react";
+import daisyui from 'daisyui';
+export default function Statistik() {
+  return (
+    <>
+    <div className="stat ">
+    <div className="stat-figure text-secondary">
+    </div>
+    <div className="stat-value text-center ">Prelisence</div>
+  </div>
+   <div className="stats shadow m-5">
+  
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+    </div>
+    <div className="stat-title">Jam Masuk</div>
+    <div className="stat-value text-xl">12:20 WIB</div>
+  </div>
+  
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+    </div>
+    <div className="stat-title">Jam Keluar</div>
+    <div className="stat-value text-xl">11:59 WIB</div>
+  </div>
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+    </div>
+    <div className="stat-desc">monday, january 10, 2024</div>
+  </div>
+  
+</div>
+<div className="stats shadow m-5">
+  
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+    </div>
+    <div className="stat-title">Jam Masuk</div>
+    <div className="stat-value text-xl">12:20 WIB</div>
+  </div>
+  
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+    </div>
+    <div className="stat-title">Jam Keluar</div>
+    <div className="stat-value text-xl">11:59 WIB</div>
+  </div>
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+    </div>
+    <div className="stat-desc">monday, january 10, 2024</div>
+  </div>
+  
+</div>
+ <div className="stats shadow m-5">
+  
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+    </div>
+    <div className="stat-title">Jam Masuk</div>
+    <div className="stat-value text-xl">12:20 WIB</div>
+  </div>
+  
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+    </div>
+    <div className="stat-title">Jam Keluar</div>
+    <div className="stat-value text-xl">11:59 WIB</div>
+  </div>
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+    </div>
+    <div className="stat-desc">monday, january 10, 2024</div>
+  </div>
+  
+</div>
+
+    </>
+  )
+}
---
+++ src/app/me/statistik/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Statistik from '@/app/components/statis/Statistik'
+export default function statistik() {
+  return (
+    <>
+    <Statistik/>
+    </>
+  )
+}
